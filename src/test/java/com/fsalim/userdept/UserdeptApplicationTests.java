4bc1a702b5dcbbbdbc7c07fa3a52de76e659a597
fahmedx <fahmecanaldx@gmail.com>
2024-09-15 20:36:53 -0300

    first commit

---
+++ src/test/java/com/fsalim/userdept/UserdeptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fsalim.userdept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserdeptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
